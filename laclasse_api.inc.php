ac9910659b6afb9fcc0860d0858ecda8132a97f7
NOC ERASME <noc@erasme.org>
2016-11-07 17:31:14 +0100

    use basic HTTP authentication

--- laclasse_api.inc.php
+++ laclasse_api.inc.php
@@ -1,45 +1,21 @@
 <?php
 
 /*
- * Fonctions diverses et variées des API
- */
-
-//function p($s) {
-//   echo htmlentities($s) . "<br/>";
-//}
-
-function generer_appel($url, $app_id, $api_key, $params) {
-   $canonical_string = $url."?app_id=".urlencode($app_id)."&app_key=".urlencode($api_key);
-//   $query_string = "";
-   // 1. trier les paramètres
-//   ksort($params);
-   // 2. construction de la canonical string
-   foreach ($params as $k => $v) $canonical_string .= "&".urlencode($k)."=".urlencode ($v);
-//   $query_string = trim($query_string, "&");
-//   $canonical_string.= $query_string;
-   // 3. ajout du timestamp
-//   $timestamp = date("Y-m-d\TH:i:s");
-//   $canonical_string .= ";".$timestamp;
-   //4. Ajout de l'identifiant d'application (connu de l'annuaire, et qui lu permet de comprendre la signature)
-//   $canonical_string .= ";".$app_id;
-   // 5. Calcul de la signature : sha1 et Encodage Base64
-//   $signature = "signature=".urlencode(base64_encode(hash_hmac('sha1', $canonical_string, $api_key, true)));
-   // Renvoie de la requete constituée
-//   $req = $url . "?" .  $query_string . ";app_id=" . $app_id . ";timestamp=" . urlencode($timestamp) . ";" . $signature;
-//   p($req);
-   return $canonical_string;
-}
-
-/*
   * Fonction d'envoi d'un GET http vers l'annuaire ENT.
   */
 function interroger_annuaire_ENT($url_api, $app_id, $api_key, $params) {
-     $url = generer_appel($url_api, $app_id, $api_key, $params);
+     $query_string = "";
+     foreach ($params as $k => $v) $query_string .= urlencode($k)."=".urlencode ($v)."&";
+     $query_string = rtrim($query_string, '&');
+     $url = $url_api."?".$query_string;
+
      $ch = curl_init();
      curl_setopt($ch, CURLOPT_URL, $url);
      curl_setopt($ch, CURLOPT_ENCODING ,"");
      curl_setopt($ch, CURLOPT_HEADER, 0);
      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($ch, CURLOPT_USERPWD, $app_id . ":" . $api_key);
+
      $data = curl_exec($ch);
      if (curl_errno($ch)) {
          return curl_error($ch);
